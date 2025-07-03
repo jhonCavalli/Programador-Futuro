a680d6686c684eee0fceb9886fb8ba508acff72e
jhon Cavalli <jhoncavalli@hotmail.com>
2025-07-02 22:37:50 -0300

    Adicionar função de clique no botão e atualizar o input para captura de animação

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+/*
+ 
+[]  descobrir quando no botao foi clicado
+[]  pegar o que foi escrito m input
+[]  enviar para o n8n
+[] recber o que o n8n respondeu  
+[] colocar na tela o que ele respondeu
+[] 
+*/
+
+
+
+
+
+function clicarNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+    let
+    //
+}
+
+
+
+
